b448b9f4501dd02fa3f7d666684213a85104691e
carlosmeisen <E149833@hugoboss.com>
2026-01-06 14:41:19 +0000

    Fix version catalog path resolution for CI environment

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -9,7 +9,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from(files("../gradle/libs.versions.toml"))
+            from(files(layout.projectDirectory.file("../gradle/libs.versions.toml")))
         }
     }
 }
